53401dbfc9400c5c39ab8bb5162f72628e1ed7b4
tsuki917 <einstrom917@gmail.com>
2023-09-23 11:19:55 +0900

    add thread

---
+++ app/frontend/src/app/components/ThreadsCom/Threads.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+export default function Threads() {
+  return <div>Threads</div>;
+}
--- app/frontend/src/app/components/Title.tsx
+++ app/frontend/src/app/components/Title.tsx
@@ -1,4 +1,4 @@
 import React from "react";
-export default function Home() {
+export default function Title() {
   return <div>test</div>;
 }
