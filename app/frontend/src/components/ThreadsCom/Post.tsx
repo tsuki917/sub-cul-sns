d4e892a425deca4ef24885773fca9f07cccfba6a
tsuki917 <einstrom917@gmail.com>
2023-12-16 12:38:19 +0900

    feat:fetch posts

--- app/frontend/src/components/ThreadsCom/Post.tsx
+++ app/frontend/src/components/ThreadsCom/Post.tsx
@@ -9,26 +9,40 @@ import {
 } from "@nextui-org/react";
 import ReplyIcon from "../Icons/ReplyIcon";
 import StarIcon from "../Icons/StarIcon";
-export default function Post() {
+
+type postType = {
+  Author: string;
+  Content: string;
+  Comments: commentType[];
+};
+type commentType = {
+  Author: string;
+  Content: string;
+};
+type Props = {
+  post: postType;
+};
+
+export default function Post(prop: Props) {
   return (
-    <Card className="border-1 border-black">
-      <CardHeader>
-        <User
-          name="Jane Doe"
-          avatarProps={{
-            src: "https://i.pravatar.cc/150?u=a04258114e29026702d",
-          }}
-        />
-      </CardHeader>
-      <CardBody className="text-xl">
-        何をやってるんだか何もわかりません。私はどうすればいいんでしょうか？あああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ
-      </CardBody>
-      <CardFooter>
-        <div className="flex justify-around border-1 w-full border-black">
-          <ReplyIcon />
-          <StarIcon />
-        </div>
-      </CardFooter>
-    </Card>
+    <div className="p-4">
+      <Card className="border-1   border-black">
+        <CardHeader>
+          <User
+            name={prop.post.Author}
+            avatarProps={{
+              src: "https://i.pravatar.cc/150?u=a04258114e29026702d",
+            }}
+          />
+        </CardHeader>
+        <CardBody className="text-xl">{prop.post.Content}</CardBody>
+        <CardFooter>
+          <div className="flex justify-start  w-full ">
+            <ReplyIcon />
+            <StarIcon />
+          </div>
+        </CardFooter>
+      </Card>
+    </div>
   );
 }
--- app/frontend/src/components/ThreadsCom/Posts.tsx
+++ app/frontend/src/components/ThreadsCom/Posts.tsx
@@ -1,32 +1,102 @@
-import React from "react";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Input,
+  User,
+} from "@nextui-org/react";
+import axios, { AxiosResponse } from "axios";
+import React, {
+  ChangeEvent,
+  FormEvent,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import ReplyIcon from "../Icons/ReplyIcon";
+import StarIcon from "../Icons/StarIcon";
+import Post from "./Post";
+type postType = {
+  Author: string;
+  Content: string;
+  Comments: commentType[];
+};
+type commentType = {
+  Author: string;
+  Content: string;
+};
+
 export default function Threads() {
-  const testData = [
-    {
-      AcountData: {
-        name: "test1",
-      },
-      postDetail: {
-        text: "よろしく",
-        image: null,
-        favo: 29,
-        repost: 2,
-      },
-    },
-    {
-      AcountData: {
-        name: "test2",
-      },
-      postDetail: {
-        text: "よろしく",
-        image: null,
-        favo: 29,
-        repost: 2,
-      },
-    },
-  ];
+  const [name, setName] = useState<string>("");
+  const [content, setContent] = useState<string>("");
+  const [posts, setPosts] = useState<postType[]>([]);
+  // const [isInit, setIsinit] = useState<boolean>(false);
+  const isInit = useRef<boolean>(false);
+  useEffect(() => {
+    if (isInit.current == false) {
+      axios
+        .get("http://localhost:8080/getallpost")
+        .then((res: AxiosResponse) => {
+          res.data.posts.forEach((element: postType) => {
+            setPosts((prev) => [...prev, element]);
+          });
+        });
+      isInit.current = true;
+    }
+  }, []);
+
+  const onAddPost = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    let url = "http://localhost:8080/createpost";
+    const content_str = "content=" + content;
+    const name_str = "author=" + name;
+    url += "?" + content_str + "&" + name_str;
+    // axios.get(url).then((res) => {
+    //   console.log(res.data.post);
+    // });
+    setContent("");
+    setName("");
+  };
+
   return (
-    <div className="ml-2">
+    <div className="pt-5 pb-5 pr-8 pl-8">
+      <div className="border-1">
+        {posts.map((post, key) => {
+          return (
+            <div className="border-1" key={key}>
+              <Post post={post} />
+            </div>
+          );
+        })}
+      </div>
+
+      <form action="post" onSubmit={onAddPost} className="border-1">
+        <label>author : </label>
+        <Input
+          type="text"
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <label> content : </label>
+        <Input
+          type="text"
+          name="content"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
       <h2>Threads</h2>
+      <Button
+        onClick={() => {
+          console.log(posts);
+        }}
+      >
+        posts
+      </Button>
     </div>
   );
 }
